3cf1850864a8f17c2612371e85b41056bbe6822f
alejoGonc <65875123+alejoGonc@users.noreply.github.com>
2024-01-06 17:38:57 -0300

    ej 1

--- 8WeekSQLChallenge/Week1/case1.sql
+++ 8WeekSQLChallenge/Week1/case1.sql
@@ -1,1 +1,7 @@
-SELECT * FROM menu
+-- What is the total amount each customer spent at the restaurant?
+
+select sales.customer_id, sum(menu.price) as total_spent
+from sales 
+inner join menu on sales.product_id = menu.product_id
+group by customer_id;
+
