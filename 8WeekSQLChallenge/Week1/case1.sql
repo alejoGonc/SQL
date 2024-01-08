78ebaaed8be9c34b7cd0fc3c844db4bd9f08a180
alejoGonc <65875123+alejoGonc@users.noreply.github.com>
2024-01-08 14:18:54 -0300

    update

--- 8WeekSQLChallenge/Week1/case1.sql
+++ 8WeekSQLChallenge/Week1/case1.sql
@@ -24,3 +24,16 @@ join menu on t.product_id = menu.product_id
 WHERE row_num = 1;
 
 -- What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+-- Which item was the most popular for each customer?
+
+-- Which item was purchased first by the customer after they became a member?
+
+-- Which item was purchased just before the customer became a member?
+
+-- What is the total items and amount spent for each member before they became a member?
+
+-- If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+-- In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not
+-- just sushi - how many points do customer A and B have at the end of January?
