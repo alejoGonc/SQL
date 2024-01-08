9c4502ab22dcca1a1938b5678f787a9ab3733083
alejoGonc <65875123+alejoGonc@users.noreply.github.com>
2024-01-08 14:17:22 -0300

    question 3

--- 8WeekSQLChallenge/Week1/case1.sql
+++ 8WeekSQLChallenge/Week1/case1.sql
@@ -9,8 +9,18 @@ group by customer_id;
 
 select sales.customer_id, COUNT(DISTINCT sales.order_date) as days_visited
 from sales
-group by customer_id
+group by customer_id;
 
 
 -- What was the first item from the menu purchased by each customer?
 
+SELECT customer_id, order_date, t.product_id ,menu.product_name
+FROM (
+    SELECT sales.customer_id, sales.order_date, sales.product_id,
+           ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS row_num
+    FROM sales
+) t
+join menu on t.product_id = menu.product_id
+WHERE row_num = 1;
+
+-- What is the most purchased item on the menu and how many times was it purchased by all customers?
