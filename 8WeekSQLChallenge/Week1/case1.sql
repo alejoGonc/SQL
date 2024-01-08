56b181318a227a0ea6b30364f795b41a29c1b417
alejoGonc <65875123+alejoGonc@users.noreply.github.com>
2024-01-08 14:02:14 -0300

    question 2

--- 8WeekSQLChallenge/Week1/case1.sql
+++ 8WeekSQLChallenge/Week1/case1.sql
@@ -5,3 +5,12 @@ from sales
 inner join menu on sales.product_id = menu.product_id
 group by customer_id;
 
+-- How many days has each customer visited the restaurant?
+
+select sales.customer_id, COUNT(DISTINCT sales.order_date) as days_visited
+from sales
+group by customer_id
+
+
+-- What was the first item from the menu purchased by each customer?
+
